0c0555d61405c3997c2609a28c55be8ecdcca176
Rotomeca <delphin.tommy@gmail.com>
2022-06-17 13:37:34 +0200

    Update csv.py

--- csv.py
+++ csv.py
@@ -1,11 +1,16 @@
-class csv:
-    def __init__(self, path, type=';', has_column=True) -> None:
+import copy
+
+
+class RImportedCsv:
+    def __init__(self, path, type=';', has_headers=True) -> None:
         self.__columns = {}
         self.__array = []
         self.__original_path = path
-        self.__import_csv(csv, type)
+        self.__has_headers = has_headers
+        if path is not None:
+            self.__import_csv(type)
 
-    def import_csv(self, csv, type):
+    def import_csv(self, type):
         file = open(self.__original_path, 'r')
         raw = file.read()
         file.close()
@@ -18,7 +23,7 @@ class csv:
         it = 0
         for item in splited_raw:
             raw_datas = item.split(type)
-            if it == 0:
+            if it == 0 and self.__has_headers:
                 i = 0
                 for d in raw_datas:
                     columns[d] = i
@@ -32,7 +37,7 @@ class csv:
 
     __import_csv = import_csv
 
-    def __getitem__(self, pos):
+    def __getitem__(self, pos) -> list | any:
         column, row = pos
         if column in self.__columns:
             column = self.__columns[column]
@@ -53,3 +58,30 @@ class csv:
 
     def row_count(self):
         return len(self.__array)
+
+    def clone(self):
+        return copy.deepcopy(self)
+
+    def get_array(self):
+        return self.__array
+
+    def get_cols(self):
+        return self.__columns
+
+    def path(self):
+        return self.__original_path
+
+
+class Rcsv(RImportedCsv):
+    def __init__(self, path=None, separator=';', has_headers=True):
+        super().__init__(path, separator, has_headers)
+        self.__imported_from_path = None
+        self.separator = separator
+
+    @staticmethod
+    def from_array(array, cols=None):
+        csv = Rcsv()
+        if cols is not None:
+            csv.__columns = cols
+        csv.__array = array
+        return csv
